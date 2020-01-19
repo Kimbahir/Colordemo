f71741f073684e2c80fb3446fd8178c2f9210b4d
Kim Bahir Andersen <kim@bahir.dk>
2020-01-19 10:13:30 +0100

    Removed legacy code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ import random
 import socket
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'df61f5fe12eb40792e85b284ead07d51'
 
 app.config['kba_counter'] = 0
 app.config['kba_color'] = (random.choice(range(256)), random.choice(
